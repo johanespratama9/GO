8ea8533776b4cb2845ccaffe471fcabcbe886737
johanespratama9 <johanespratama09@gmail.com>
2023-02-22 11:57:00 +0700

    slice

---
+++ slice/slice.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var gamingconsole []string
+	// gamingconsole = append(gamingconsole, "playstation")
+	// gamingconsole = append(gamingconsole, "XBOX")
+	// gamingconsole = append(gamingconsole, "pSP")
+
+	gamingconsole := []string{"psp", "playstation"}
+
+	fmt.Println(gamingconsole)
+}
